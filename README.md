ea365458cf948eb936ab43f79e69968c8e0adf3a
pathofleastresistor <byamitvaria@gmail.com>
2024-02-18 12:45:35 -0800

    Add RUN_AT_STARTUP and update README.md

--- README.md
+++ README.md
@@ -5,5 +5,12 @@ This repository allows the user to update A records on Cloudflare to current IP 
 2. Create a token with Zone:Zone:Read and Zone:DNS:Edit
 3. Copy .env.default to .env and replace INSERT_TOKEN_HERE with your token.
 
+### Set schedule
+You need to set the CRON_SCHEDULE environment variable so that the script will check on a regular basis if the IP address is current. The default is set to every 5 minutes.
+
+#### Run at startup
+By default, when the user starts the container, the update script will run. To avoid this, set the env variable RUN_AT_STARTUP to `false`.
+
+
 ### Running the container
 Run `docker compose up --build -d`.
--- startup.sh
+++ startup.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
-# Run your Python script once at startup
-python3 /usr/src/app/update.py
+# Default to running the script at startup unless RUN_AT_STARTUP is set to "false"
+if [ "$RUN_AT_STARTUP" != "false" ]; then
+  echo "Running script at startup..."
+  python3 /usr/src/app/update.py
+else
+  echo "Skipping script run at startup..."
+fi
+
+# Set a default cron schedule if CRON_SCHEDULE is not set
+CRON_SCHEDULE="${CRON_SCHEDULE:-*/5 * * * *}"
 
 # Create a new crontab entry using the CRON_SCHEDULE environment variable
 echo "$CRON_SCHEDULE /usr/local/bin/python /usr/src/app/update.py >> /var/log/cron.log 2>&1" | crontab -
 
 # Start the cron service in the foreground
-# Note: The specific command to start cron depends on your base image
 cron -f
